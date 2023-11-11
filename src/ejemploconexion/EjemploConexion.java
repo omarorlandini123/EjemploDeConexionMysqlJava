3cb6abf307c3834b4f567fc19ddaa0ec4c463c89
omarorlandini123 <omarorlandini123@gmail.com>
2023-11-11 11:39:51 -0500

    Segundo comentario

--- src/ejemploconexion/EjemploConexion.java
+++ src/ejemploconexion/EjemploConexion.java
@@ -14,7 +14,7 @@ public class EjemploConexion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Primer commit
     }
     
 }
